db253ac95bfa4363cbacb3997bf7e392ea606d7f
Mateusz <maniekwroc@gmail.com>
2021-07-15 11:04:26 +0200

    update driver

--- src/main/java/manager/DriverManager.java
+++ src/main/java/manager/DriverManager.java
@@ -40,15 +40,15 @@ public class DriverManager {
     private static WebDriver createDriver() {
         switch (browserType) {
             case FIREFOX:
-                System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
+                System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
                 driver = new FirefoxDriver();
                 break;
             case CHROME:
-                System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+                System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
                 driver = new ChromeDriver();
                 break;
             case EDGE:
-                System.setProperty("webdriver.edge.driver", "msedgedriver.exe");
+                System.setProperty("webdriver.edge.driver", "drivers/msedgedriver.exe");
                 driver = new EdgeDriver();
                 break;
         }
